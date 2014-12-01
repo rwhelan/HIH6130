04d643b49bd00be2a48fb917f0ad46cfe3ab347a
Ryan Whelan <rcwhelan@gmail.com>
2014-12-01 12:25:38 -0500

    Added Access-Control-Allow-Origin to server headers

--- LocalWebServer.go
+++ LocalWebServer.go
@@ -98,6 +98,7 @@ func webHandler(w http.ResponseWriter, r *http.Request) {
                 return
         }
         w.Header().Set("Content-Type", "application/json")
+        w.Header().Set("Access-Control-Allow-Origin", "*")
         w.Write(js)
 }
 
